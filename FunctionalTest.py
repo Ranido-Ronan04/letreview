b6591de55006e51fd4722ff5ee1b9f673a09c9fe
Ronan Kyle Ranido <ronankyle.ranido@gsfe.tupcavite.edu.ph>
2021-04-06 19:13:36 +0800

    add

--- FunctionalTest.py
+++ FunctionalTest.py
@@ -3,27 +3,27 @@ import unittest
 from selenium.webdriver.common.keys import Keys
 import time
 
-"""browser = webdriver.Firefox()
-browser.get('http://127.0.0.1:8000')"""
+browser = webdriver.Firefox()
+#browser.get('http://127.0.0.1:8000')
 
 class PageTest(unittest.TestCase):
 
-	def setUp(self):
-		self.browser = webdriver.Firefox()
+	def setup(self):
+		self.browser = webdriver.firefox()
 
 	"""def tearDown(self):
 		self.browser.quit()"""
 
 	def test_browser_test(self):
-		"""self.browser.get('http://localhost:8000')
-		self.assertIn('BTS', self.browser.title)"""
+		self.browser.get('http://localhost:8000')
+		"""self.assertIn('BTS', self.browser.title)"""
 		#self.fail('Finish the test!')
 
 	def test_start_list_and_retrieve_it(self):
 		self.browser.get('http://localhost:8000')
 		"""self.assertIn('KPOP Group', self.browser.title)"""
 		headerText = self.browser.find_element_by_tag_name('h1').text
-		self.assertIn('Group', headerText)
+		self.assertIn('Students Profile', headerText)
 		inputbox = self.browser.find_element_by_id('idNewEntry').send_keys("BTS")
 		#self.assertEqual(inputbox.get_attribute('placeholder'),'Group name you have seen.')
 		#inputbox.send_keys('BTS')
